f80f883a543afeac4e7055144c2ea3e26af44ccf
Alexey Androsov <doochik@ya.ru>
2012-06-05 15:54:43 +0400

    Update master

--- jquery.looptab.js
+++ jquery.looptab.js
@@ -4,7 +4,7 @@
      * Capture tab key navigation inside given element
      * @see https://github.com/doochik/jquery.looptab.js
      * @author Alexey Androsov <doochik@ya.ru>
-     * @licence GPLv3
+     * @licence GPLv3/MIT
      * @version 0.1
      */
 
